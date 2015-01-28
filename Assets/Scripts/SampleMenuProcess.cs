d7d9ec9e526d9c25499afbd2ef763b91141abf52
ms <ms.shao@dygame.cn>
2015-01-28 17:25:55 +0800

    add files and commit

---
+++ Assets/Scripts/SampleMenuProcess.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+using System.Collections;
+using System ;//for BitConverter
+
+public enum ENUM_UPGRADE_STATE
+{
+	NONE,
+	EXPERIENCE,
+	LEVEL,
+	MONEY,
+	RATIO,
+	EMPTY,
+	MAX
+}
+
+public class SampleMenuProcess : MonoBehaviour
+{
+	public		Transform 		uiCamera	= null ;
+	public		Transform 		MenuRoot	= null ;
+	public		Transform 		MenuNewTest	= null ;
+	protected	GameObject[] 	UpgradeExp	;
+	protected	GameObject[] 	UpgradeLevel;
+	protected	GameObject[] 	UpgradeMoney;
+	protected	GameObject[] 	UpgradeRatio;
+	protected	GameObject[] 	UpgradeEmpty;
+	//Init
+	void Awake()
+	{
+		UpgradeMoney	= new GameObject[4] ;
+		UpgradeMoney[0] = MenuRoot.Find("Money/GameObject0").gameObject;
+		UpgradeMoney[1] = MenuRoot.Find("Money/GameObject1").gameObject;
+		UpgradeMoney[2] = MenuRoot.Find("Money/GameObject2").gameObject;
+		UpgradeMoney[3] = MenuRoot.Find("Money/GameObject3").gameObject;
+		UpgradeExp		= new GameObject[4] ;
+		UpgradeExp[0]	= MenuRoot.Find("Experience/GameObject0").gameObject;
+		UpgradeExp[1]	= MenuRoot.Find("Experience/GameObject1").gameObject;
+		UpgradeExp[2]	= MenuRoot.Find("Experience/GameObject2").gameObject;
+		UpgradeExp[3]	= MenuRoot.Find("Experience/GameObject3").gameObject;
+		UpgradeLevel	= new GameObject[4] ;
+		UpgradeLevel[0] = MenuRoot.Find("Level/GameObject0").gameObject;
+		UpgradeLevel[1] = MenuRoot.Find("Level/GameObject1").gameObject;
+		UpgradeLevel[2] = MenuRoot.Find("Level/GameObject2").gameObject;
+		UpgradeLevel[3] = MenuRoot.Find("Level/GameObject3").gameObject;
+		UpgradeRatio	= new GameObject[4] ;
+		UpgradeRatio[0] = MenuNewTest.Find("Ratio/GameObject0").gameObject;
+		UpgradeRatio[1] = MenuNewTest.Find("Ratio/GameObject1").gameObject;
+		UpgradeRatio[2] = MenuNewTest.Find("Ratio/GameObject2").gameObject;
+		UpgradeRatio[3] = MenuNewTest.Find("Ratio/GameObject3").gameObject;
+		UpgradeEmpty	= new GameObject[4] ;
+		UpgradeEmpty[0] = MenuNewTest.Find("Empty/GameObject0").gameObject;
+		UpgradeEmpty[1] = MenuNewTest.Find("Empty/GameObject1").gameObject;
+		UpgradeEmpty[2] = MenuNewTest.Find("Empty/GameObject2").gameObject;
+		UpgradeEmpty[3] = MenuNewTest.Find("Empty/GameObject3").gameObject;
+		//創建全局數字sprite庫prefab.
+		GameObject obj = GameObject.Find("SpriteRendererUtility(DontDestroy)");
+		if( !obj ) SpriteRendererUtility.SpriteRendererObject = (GameObject)Instantiate(Resources.Load("Prefab/SpriteRendererUtility"));
+	}
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	//
+	void OnGUI()
+	{	
+		if (GUI.Button(new Rect(10, 110 , 150, 30), "Random!"))
+		{ 
+			int iRandomExp	= UnityEngine.Random.Range(0, 10000);
+			int iRandomLevel= UnityEngine.Random.Range(0, 10000);
+			int iRandomMoney= UnityEngine.Random.Range(0, 10000);
+			int iRandomRatio= UnityEngine.Random.Range(0, 10000);
+			SetUpgradeState(ENUM_UPGRADE_STATE.EXPERIENCE,iRandomExp,	0);
+			SetUpgradeState(ENUM_UPGRADE_STATE.LEVEL	,iRandomLevel,	0);
+			SetUpgradeState(ENUM_UPGRADE_STATE.MONEY	,iRandomMoney,	0);
+			SetUpgradeState(ENUM_UPGRADE_STATE.RATIO	,iRandomRatio,	0);
+		}
+		if (GUI.Button(new Rect(10, 150 , 150, 30), "RandomII"))
+		{ 
+			int iRandomRatio= UnityEngine.Random.Range(0, 10000);
+			int iRandomFont	= UnityEngine.Random.Range(0, 5);
+			SetUpgradeState(ENUM_UPGRADE_STATE.RATIO	,iRandomRatio,	iRandomFont);
+		}
+		if (GUI.Button(new Rect(10, 190 , 150, 30), "RandomIII"))
+		{ 
+			int iRandomEmpty= UnityEngine.Random.Range(0, 10000);
+			int iRandomFont	= UnityEngine.Random.Range(0, 5);
+			SetUpgradeState(ENUM_UPGRADE_STATE.EMPTY	,iRandomEmpty,	iRandomFont);
+		}
+		//Test DontDestroyOnLoad()
+		if (GUI.Button(new Rect(160, 110 , 150, 30), "Main Scene!"))
+		{ 
+			Application.LoadLevel("main") ;
+		}
+		if (GUI.Button(new Rect(160, 150 , 150, 30), "Empty Scene!"))
+		{ 
+			Application.LoadLevel("test") ;
+		}
+	}
+	//	
+	void SetUpgradeState(ENUM_UPGRADE_STATE state, int iIndex , int iOption)
+	{
+		switch( state )
+		{
+			case ENUM_UPGRADE_STATE.NONE: break;
+			case ENUM_UPGRADE_STATE.EXPERIENCE:
+			{
+				if ((iIndex > 9999) || (iIndex < 0)) iIndex = 0 ;//out of range
+				string sResult = Convert.ToString(iIndex) ;
+				int iMaxDigit = 4 ;//位數上限
+				for (int i = 0 ; i < iMaxDigit ; i++) UpgradeExp[i].SetActive(true);//reset
+				int iCount = 0 ;
+				for (int i = 0 ; i < sResult.Length ; i++)
+				{
+					string s = "" + sResult[sResult.Length-1-i] ;//從此字串個位數開始
+					int vOut = int.Parse(s);
+					if (i >= iMaxDigit) break ;//防當
+					UpgradeExp[iMaxDigit-1-i].GetComponent<JSK_MenuObject>().setMenuState(JSK_MenuState.ENUM_SPRITE_INDEX, vOut);//從畫面上的個位數開始
+					iCount++ ;
+				}
+				for (/*    */; iCount < iMaxDigit ; iCount++)
+				{
+					UpgradeExp[iMaxDigit-1-iCount].GetComponent<JSK_MenuObject>().setMenuState(JSK_MenuState.ENUM_SPRITE_INDEX, 0);//從畫面上的個位數開始補0
+				}
+				break ;
+			}
+			case ENUM_UPGRADE_STATE.LEVEL:
+			{
+				if ((iIndex > 9999) || (iIndex < 0)) iIndex = 0 ;//out of range
+				string sResult = Convert.ToString(iIndex) ;
+				int iMaxDigit = 4 ;//位數上限
+				for (int i = 0 ; i < iMaxDigit ; i++) UpgradeLevel[i].SetActive(true);//reset
+				int iCount = 0 ;
+				for (int i = 0 ; i < sResult.Length ; i++)
+				{
+					string s = "" + sResult[i] ;//從此字串最高位數開始
+					int vOut = int.Parse(s);
+					if (i >= iMaxDigit) break ;//防當
+					UpgradeLevel[i].GetComponent<JSK_MenuObject>().setMenuState(JSK_MenuState.ENUM_SPRITE_INDEX, vOut);//從畫面上的最高位數開始
+					iCount++ ;
+				}
+				for (/*    */; iCount < iMaxDigit ; iCount++)
+				{
+					UpgradeLevel[iCount].SetActive(false);//把未顯示的位數Hide
+				}
+				break ;
+			}
+			case ENUM_UPGRADE_STATE.MONEY:
+			{
+				if ((iIndex > 9999) || (iIndex < 0)) iIndex = 0 ;//out of range
+				string sResult = Convert.ToString(iIndex) ;
+				int iMaxDigit = 4 ;//位數上限
+				for (int i = 0 ; i < iMaxDigit ; i++) UpgradeMoney[i].SetActive(true);//reset
+				int iCount = 0 ;
+				for (int i = 0 ; i < sResult.Length ; i++)
+				{
+					string s = "" + sResult[sResult.Length-1-i] ;//從此字串個位數開始
+					int vOut = int.Parse(s);
+					if (i >= iMaxDigit) break ;//防當
+					UpgradeMoney[iMaxDigit-1-i].GetComponent<JSK_MenuObject>().setMenuState(JSK_MenuState.ENUM_SPRITE_INDEX, vOut);//從畫面上的個位數開始
+					iCount++ ;
+				}
+				for (/*    */; iCount < iMaxDigit ; iCount++)
+				{
+					UpgradeMoney[iMaxDigit-1-iCount].SetActive(false);//把未顯示的位數Hide
+				}
+				break ;
+			}
+			case ENUM_UPGRADE_STATE.RATIO:
+			{
+				if ((iIndex > 9999) || (iIndex < 0)) iIndex = 0 ;//out of range
+				string sResult = Convert.ToString(iIndex) ;
+				int iMaxDigit = 4 ;//位數上限
+				for (int i = 0 ; i < iMaxDigit ; i++) UpgradeRatio[i].SetActive(true);//reset
+				int iCount = 0 ;
+				for (int i = 0 ; i < sResult.Length ; i++)
+				{
+					string s = "" + sResult[sResult.Length-1-i] ;//從此字串個位數開始
+					int vOut = int.Parse(s);
+					if (i >= iMaxDigit) break ;//防當
+					SpriteRendererUtility.Instance().SetGameObjectSprite(UpgradeRatio[iMaxDigit-1-i] , vOut , iOption) ;
+					iCount++ ;
+				}
+				for (/*    */; iCount < iMaxDigit ; iCount++)
+				{
+					UpgradeRatio[iMaxDigit-1-iCount].SetActive(false);//把未顯示的位數Hide
+				}
+				break ;
+			}
+			case ENUM_UPGRADE_STATE.EMPTY:
+			{
+				int iRandomBehaviour = UnityEngine.Random.Range(0, 3);
+				SpriteRendererUtility.Instance().SetGameObjectNumber(UpgradeEmpty , 359 , 4 , iOption , iRandomBehaviour) ;
+				break ;
+			}
+			default:break ;
+		}
+	}
+}
---
+++ Assets/Scripts/SpriteRendererUtility.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System ;//for BitConverter
+
+public enum ENUM_SPRITE_RENDERER_BEHAVIOUR : int
+{
+	FILL_ZERO = 0,			//最高位數為0時補0,(沒有個位數為0時補0這種東西),	Ex:"0359"
+	LEFT_ZERO_DISABLE = 1,	//從最高位數開始,最高位數為0時disable它,		Ex:" 359"
+	RIGHT_ZERO_DISABLE = 2,	//從個位數開始,最高位數為0時disable它,			Ex:"359 "
+	MAX
+}
+
+public enum ENUM_SPRITE_PACKAGE : int
+{
+	WHITE		= 0,
+	BLUE		= 1,
+	WHITE_BLUE	= 2,
+	LIGHT_YELLOW= 3,
+	DARK_YELLOW	= 4,
+	MAX
+}
+
+public class SpriteRendererUtility : MonoBehaviour 
+{
+	public	Sprite[]	SpriteListWhite		= null;
+	public	Sprite[]	SpriteListBlue		= null;
+	public	Sprite[]	SpriteListWhiteBlue	= null;
+	public	Sprite[]	SpriteListLightYellow= null;
+	public	Sprite[]	SpriteListDarkYellow= null;
+	//self
+	public	static		GameObject	SpriteRendererObject = null ;//指向自己的prefab,不保存也沒關係?
+	protected static 	SpriteRendererUtility Self;
+	protected Sprite 	SpriteTemp = null ;
+	//
+	//Init
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);//保護自己不被刪除
+		gameObject.name = "SpriteRendererUtility(DontDestroy)" ;
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		Self = this;
+	}	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	//Instance
+	public static SpriteRendererUtility Instance()
+	{
+		return Self;
+	}
+	//Set GameObject 的 SpriteRenderer 給指定 value 的數字
+	public void SetGameObjectSprite(GameObject gameObject , int iValue , int iSpritePackageIndex)
+	{
+		switch( iSpritePackageIndex )
+		{
+			case (int)ENUM_SPRITE_PACKAGE.WHITE: SpriteTemp = SpriteListWhite[iValue]; break ;
+			case (int)ENUM_SPRITE_PACKAGE.BLUE: SpriteTemp = SpriteListBlue[iValue]; break ;
+			case (int)ENUM_SPRITE_PACKAGE.WHITE_BLUE: SpriteTemp = SpriteListWhiteBlue[iValue]; break ;
+			case (int)ENUM_SPRITE_PACKAGE.LIGHT_YELLOW: SpriteTemp = SpriteListLightYellow[iValue]; break ;
+			case (int)ENUM_SPRITE_PACKAGE.DARK_YELLOW: SpriteTemp = SpriteListDarkYellow[iValue]; break ;
+			default:SpriteTemp = SpriteListWhite[iValue]; break ;
+		}
+		//set sprite
+		gameObject.transform.GetComponent<SpriteRenderer>().sprite = SpriteTemp ;
+	}
+	//
+	public void SetGameObjectNumber(GameObject[] gameObject , int iValue , int iDigit , int iSpritePackageIndex , int iBehaviour)
+	{
+		int iMaxDigit = iDigit ;//位數上限
+		int iLimit = 1 ;
+		for (int i = 0 ; i < iMaxDigit ; i++) iLimit *= 10 ;
+		if ((iValue >= iLimit) || (iValue < 0)) iValue = 0 ;//out of range
+		string sResult = Convert.ToString(iValue) ;
+		if (sResult.Length > iMaxDigit)
+			return ;//不可能出現?
+		else if (sResult.Length == iMaxDigit)
+		{
+			for (int i = 0 ; i < iMaxDigit ; i++)
+			{
+				gameObject[i].SetActive(true);//reset enable
+				string s = "" + sResult[i] ;//位數與欄位數相符,從那邊開始都可
+				int iOut = int.Parse(s);
+				Instance().SetGameObjectSprite(gameObject[i] , iOut , iSpritePackageIndex) ;
+			}
+		}
+		else
+		{
+			for (int i = 0 ; i < iMaxDigit ; i++) gameObject[i].SetActive(true);//reset enable
+			//
+			int iFillCount = 0 ;
+			if (iBehaviour == (int)ENUM_SPRITE_RENDERER_BEHAVIOUR.FILL_ZERO)//最高位數為0時補0
+			{
+				string sTemp  = sResult.PadLeft(iDigit, '0');//高位數,不足補0
+				sResult = sTemp ;
+			}
+			else if (iBehaviour == (int)ENUM_SPRITE_RENDERER_BEHAVIOUR.LEFT_ZERO_DISABLE)//從最高位數開始,最高位數為0時disable它		
+			{
+				iFillCount = iMaxDigit - sResult.Length ;
+			}
+			else if (iBehaviour == (int)ENUM_SPRITE_RENDERER_BEHAVIOUR.RIGHT_ZERO_DISABLE)//從個位數開始,最高位數為0時disable它
+			{
+				iFillCount = 0 ;
+			}
+			int iCount = 0 ;
+			for (int i = 0 ; i < iFillCount ; i++) 
+			{
+				gameObject[i].SetActive(false);//從最高位數開始,把未顯示的位數Hide
+				iCount++ ;
+			}
+			for (int i = 0 ; i < sResult.Length ; i++)
+			{
+				string s = "" + sResult[i] ;
+				int iOut = int.Parse(s);
+				Instance().SetGameObjectSprite(gameObject[iFillCount+i] , iOut , iSpritePackageIndex) ;
+				iCount++ ;
+			}
+			for (/*    */; iCount < iMaxDigit ; iCount++)
+			{
+				gameObject[iCount].SetActive(false);//把未顯示的位數Hide
+			}
+		}
+	}
+}
